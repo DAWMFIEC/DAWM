1b966626fbcb8b2bb8f7467bd8e5c4626c13432a
Allan <aavendan@gmail.com>
2022-10-03 21:33:22 -0500

    C01/EJ01

--- Capítulo 01/ejercicio01/test/titulonivel1.test.js
+++ Capítulo 01/ejercicio01/test/titulonivel1.test.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const request = require('supertest');
 const app = require('../app')
 
-describe('Test unitarios para la ruta `/`', function() {
+describe('Test unitarios para la ruta `/` con títulos', function() {
 
     it('Respuesta del servidor', function() {
       return request(app)
@@ -13,27 +13,43 @@ describe('Test unitarios para la ruta `/`', function() {
         })
     });
 
-    it('Etiqueta <h1> en views/index.ejs', function() {
+    let h1 = `<h1>El título más importante</h1>`
+    let h2 = `<h2>Título secundario</h2>`
+    let h6 = `<h6>El título menos importante</h6>`
+
+    it('En views/index.ejs use la etiqueta: '+h1, function() {
       return request(app)
         .get('/')
         .then((response) => {
-            chai.expect(response.text).to.contain('<h1>El título más importante</h1>');
+            
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+            let h1clean = h1.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+            chai.expect(responseclean).to.contain(h1clean);
+
         })
     });
 
-    it('Etiqueta <h2> en views/index.ejs', function() {
+    it('En views/index.ejs use la etiqueta: '+h2, function() {
       return request(app)
         .get('/')
         .then((response) => {
-            chai.expect(response.text).to.contain('<h2>Título secundario</h2>');
+
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+            let h2clean = h2.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+            chai.expect(responseclean).to.contain(h2clean);
         })
     });
 
-    it('Etiqueta <h6> en views/index.ejs', function() {
+    it('En views/index.ejs use la etiqueta: '+h6, function() {
       return request(app)
         .get('/')
         .then((response) => {
-            chai.expect(response.text).to.contain('<h6>El título menos importante</h6>');
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+            let h6clean = h6.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+            chai.expect(responseclean).to.contain(h6clean);
         })
     });
 
