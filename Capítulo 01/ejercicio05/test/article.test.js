96124e873e5b6dc0d806dae9b8ba899390c5fa96
Allan <aavendan@gmail.com>
2022-10-06 13:42:05 -0500

    C01/EJ05

--- Capítulo 01/ejercicio05/test/article.test.js
+++ Capítulo 01/ejercicio05/test/article.test.js
@@ -12,30 +12,32 @@ describe('Test unitarios para la ruta `/`', function() {
             assert.equal(response.status, 200)
         })
     });
+    
+    let article = `
+      <article>
+        <img
+          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png"
+          alt="GeeksforGeeks"
+        >
+        <h2>GeeksforGeeks</h2>
+        <p>
+          Sandeep Jain(FOUNDER) An IIT Roorkee alumnus and 
+          founder of GeeksforGeeks. Apart from GeeksforGeeks, 
+          he has worked with DE Shaw and Co. as a software 
+          developer and JIIT Noida as an assistant professor.
+        </p>
+      </article>
+    `
 
-    it('Modifique el archivo views/index.ejs', function() {
+    it('En views/index.ejs use las etiquetas: '+article, function() {
       return request(app)
         .get('/')
         .then((response) => {
-          let responseclean = response.text.replace(/(\r\n|\n|\r)/gm, '')
-
-          let article = `
-            <article>
-              <img
-                src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png"
-                alt="GeeksforGeeks"
-              >
-              <h2>GeeksforGeeks</h2>
-              <p>
-                Sandeep Jain(FOUNDER) An IIT Roorkee alumnus and 
-                founder of GeeksforGeeks. Apart from GeeksforGeeks, 
-                he has worked with DE Shaw and Co. as a software 
-                developer and JIIT Noida as an assistant professor.
-              </p>
-            </article>
-          `
-          let articleclean = article.replace(/(\r\n|\n|\r)/gm, '')
-          chai.expect(responseclean).to.contain(articleclean);
+          
+          let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          let typetextclean = article.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          
+          chai.expect(responseclean).to.contain(typetextclean);
             
         })
     });
