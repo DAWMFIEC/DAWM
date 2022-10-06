210d91d723a56d4ac55869764cb55a63b2fb96c4
Allan <aavendan@gmail.com>
2022-10-06 13:35:02 -0500

    C01/EJ04

--- Capítulo 01/ejercicio04/test/formulario.test.js
+++ Capítulo 01/ejercicio04/test/formulario.test.js
@@ -18,6 +18,7 @@ describe('Test unitarios para la ruta `/`', function() {
     let typetext = `<label for="nombrescompletos">Nombres completos:</label> <input type="text" id="nombrescompletos" name="nombrescompletos" required="required" />`
     let typeemail = `<label for="correoelectronico">Correo electrónico:</label> <input type="email" id="correoelectronico" name="correoelectronico" required="required" />`
     let typegenre = `
+        <label>Género:</label>
         <input type="radio" id="femenino" name="genero" value="femenino">
         <label for="femenino">Femenino</label>
         <input type="radio" id="masculino" name="genero" value="masculino">
@@ -27,13 +28,22 @@ describe('Test unitarios para la ruta `/`', function() {
 
     let typedate = `<label for="fechanacimiento">Fecha de nacimiento:</label> <input type="date" id="fechanacimiento" name="fechanacimiento" required="required" />`
 
-    // let typedate = `type="date"`
-    // let typenumber = `type="number"`
-    // let typecheckbox = `type="checkbox"`
-    // let typeselect = `<select`
-    // let typetextarea = `<textarea`
-    // let typebutton1 = `<button`
-    // let typebutton2 = `</button>`
+    let select = `
+        <label for="sistemaoperativo">Sistema Operativo:</label>
+        <select id="sistemaoperativo" name="sistemaoperativo">
+            <option value="windows">Windows</option>
+            <option value="linux">Linux</option>
+            <option value="macos">MacOS</option>
+        </select>`
+
+    let typenumber = `<label for="puntuacion">Puntuación:</label> <input type="number" id="puntuacion" name="puntuacion" min="1" max="10" />`
+
+    let textarea = `<label for="mensaje">Mensaje:</label> <textarea id="mensaje" name="mensaje" rows="10" cols="50"></textarea>`
+
+    let typecheckbox = `<input type="checkbox" id="nosoyunrobot" value="nosoyunrobot"> <label for="nosoyunrobot">No soy un robot</label>`
+
+    let buttons = `<button type="submit" name="enviar">Enviar</button> <button type="reset" name="reiniciar">Cancelar</button>`
+
     let form2 = `</form>`
 
     it('En views/index.ejs use las etiquetas: '+form1+' y '+ form2, function() {
@@ -99,6 +109,74 @@ describe('Test unitarios para la ruta `/`', function() {
 
           let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
           let typetextclean = typedate.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          
+          chai.expect(responseclean).to.contain(typetextclean);
+            
+        })
+    });
+
+    it('En views/index.ejs use las etiquetas: '+select, function() {
+      return request(app)
+        .get('/')
+        .then((response) => {
+
+          let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          let typetextclean = select.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          
+          chai.expect(responseclean).to.contain(typetextclean);
+            
+        })
+    });
+
+
+    it('En views/index.ejs use las etiquetas: '+typenumber, function() {
+      return request(app)
+        .get('/')
+        .then((response) => {
+
+          let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          let typetextclean = typenumber.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          
+          chai.expect(responseclean).to.contain(typetextclean);
+            
+        })
+    });
+
+
+    it('En views/index.ejs use las etiquetas: '+textarea, function() {
+      return request(app)
+        .get('/')
+        .then((response) => {
+
+          let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          let typetextclean = textarea.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          
+          chai.expect(responseclean).to.contain(typetextclean);
+            
+        })
+    });
+
+    it('En views/index.ejs use las etiquetas: '+typecheckbox, function() {
+      return request(app)
+        .get('/')
+        .then((response) => {
+
+          let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          let typetextclean = typecheckbox.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          
+          chai.expect(responseclean).to.contain(typetextclean);
+            
+        })
+    });
+
+
+    it('En views/index.ejs use las etiquetas: '+buttons, function() {
+      return request(app)
+        .get('/')
+        .then((response) => {
+
+          let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+          let typetextclean = buttons.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
           
           chai.expect(responseclean).to.contain(typetextclean);
             
