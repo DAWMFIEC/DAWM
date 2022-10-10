0f6b685adbaa8d520d05142945d410063a6b6f9e
Allan <aavendan@gmail.com>
2022-10-10 12:49:21 -0500

    C01/EJ09

--- Capítulo 01/ejercicio09/test/css.test.js
+++ Capítulo 01/ejercicio09/test/css.test.js
@@ -16,36 +16,73 @@ describe('Test unitarios para la ruta `/`', function() {
         })
     });
 
-    it('Modifique el archivo public/stylesheets/ejercicio.css Flexbox', function() {
+    let selector1 = '#bloque1'
+    let displayname = 'display', displayvalue = 'flex'
+    let flexdirectionname = 'flex-direction', flexdirectionvalue = 'row-reverse'
+    let justifycontentname = 'justify-content', justifycontentvalue = 'space-evenly'
 
+    let selector2 = '#bloque1 img:nth-child(4)'
+    let ordername = 'order', ordervalue = '-1'
+
+
+    let selector3 = '#bloque2'
+    let displayname2 = 'display', displayvalue2 = 'grid'
+    let gridtemplatecolumnsname = 'grid-template-columns', gridtemplatecolumnsvalue = '1fr 1fr'
+    let justifyitemsname = 'justify-items', justifyitemsvalue = 'center'
+    let gridrowgapname = 'grid-row-gap', gridrowgapvalue = '10px'
+
+
+    it(`En public/stylesheets/ejercicio.css utilice el/los selectores con la(s) propiedades y valor(es):
+        ${selector1} {
+          ${displayname}: ${displayvalue};
+          ${flexdirectionname}: ${flexdirectionvalue};
+          ${justifycontentname}: ${justifycontentvalue};
+        }`, function() {
       return request(app)
         .get('/stylesheets/ejercicio.css')
         .then((response) => {
-
-          chai.expect(response.text).to.have.rule('#bloque1')
-           .and.decl('display','flex')
-           .and.decl('flex-direction','row-reverse')
-           .and.decl('justify-content','space-evenly')
-
-
-          chai.expect(response.text).to.have.rule('#bloque1 img:nth-child(4)')
-           .and.decl('order','-1')
+          
+          chai.expect(response.text).to.have.rule(selector1)
+            .and.decl( displayname, displayvalue )
+            .and.decl( flexdirectionname, flexdirectionvalue)
+            .and.decl( justifycontentname, justifycontentvalue )
+            
         })
     });
 
-    it('Modifique el archivo public/stylesheets/ejercicio.css Flexbox', function() {
-
+     it(`En public/stylesheets/ejercicio.css utilice el/los selectores con la(s) propiedades y valor(es):
+        ${selector2} {
+          ${ordername}: ${ordervalue};
+        }`, function() {
       return request(app)
         .get('/stylesheets/ejercicio.css')
         .then((response) => {
-
-          chai.expect(response.text).to.have.rule('#bloque2')
-           .and.decl('display','grid')
-           .and.decl('grid-template-columns','1fr 1fr')
-           .and.decl('justify-items','center')
-           .and.decl('grid-row-gap','10px')
+          
+          chai.expect(response.text).to.have.rule(selector2)
+            .and.decl( ordername, ordervalue )
+            
         })
     });
-   
+
+    it(`En public/stylesheets/ejercicio.css utilice el/los selectores con la(s) propiedades y valor(es):
+        ${selector3} {
+          ${displayname2}: ${displayvalue2};
+          ${gridtemplatecolumnsname}: ${gridtemplatecolumnsvalue};
+          ${justifyitemsname}: ${justifyitemsvalue};
+          ${gridrowgapname}: ${gridrowgapvalue};
+        }`, function() {
+      return request(app)
+        .get('/stylesheets/ejercicio.css')
+        .then((response) => {
+          
+          chai.expect(response.text).to.have.rule(selector3)
+            .and.decl( displayname2, displayvalue2 )
+            .and.decl( gridtemplatecolumnsname, gridtemplatecolumnsvalue)
+            .and.decl( justifyitemsname, justifyitemsvalue )
+            .and.decl( gridrowgapname, gridrowgapvalue )
+            
+        })
+    });
+
 
 });
