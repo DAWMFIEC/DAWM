b5f45a94db8f4ec1a60e68bf497080bc7bbe78f5
Allan <aavendan@gmail.com>
2022-10-27 13:22:15 -0500

    C03/EJ04

--- Capítulo 03/ejercicio04/test/ejercicio.test.js
+++ Capítulo 03/ejercicio04/test/ejercicio.test.js
@@ -21,110 +21,128 @@ describe('Test unitarios para la ruta `/`', function() {
         })
     });
 
-    it('getElementById', function() {
+    let script1 = [`elemento = document.getElementById("nombre");`,`elemento.textContent = 'Muhammed Erdem'`]
+    let script2 = [`arreglo = document.getElementsByClassName('profile-card-loc__txt')`,`elemento = arreglo[0]`, `elemento.textContent = 'Istanbul, Turkey'`]
+    let script3 = [`arreglo = document.getElementsByTagName('img')`,`elemento = arreglo[0]`, `elemento.setAttribute('src','https://res.cloudinary.com/muhammederdem/image/upload/v1537638518/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.jpg')`]
+    let script4 = [`elemento = document.querySelector('div.profile-card-inf')`,` elemento.innerHTML = textoHTML`]
+
+    it(`En public/scripts/ejercicio.js con el código:
+
+      ${script1.join(' ')}
+      
+      `, function() {
       return request(app)
         .get('/scripts/ejercicio.js')
         .then((response) => {
 
-         let responseClean = response.text.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,'')
-         let scriptTexto, scriptEjecutable
+          try {
 
-         
-         /* Revisión textual */
-         try {
-            scriptTexto = [`elemento = document.getElementById("nombre");`,`elemento.textContent = 'Muhammed Erdem'`]
-            for(let elemento of scriptTexto) {
-              chai.expect(responseClean).to.contain(elemento.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,''))
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+            
+            for(let elemento of script1) {
+              chai.expect(responseclean).to.contain(elemento.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,''))
             }
             
           } catch (error) {
-            chai.expect.fail('Revise la petición por identificador y la modificación del contenido del elemento');
+            chai.expect.fail(`Código esperado: 
+              
+              ${script1.join(' ')}
+              
+              `);
             return;
           }
-
-            
+        
         })
-    }); 
+    });
 
+    it(`En public/scripts/ejercicio.js con el código:
 
-    it('getElementsByClassName', function() {
+      ${script2.join('\n')}
+      
+      `, function() {
       return request(app)
         .get('/scripts/ejercicio.js')
         .then((response) => {
 
-         let responseClean = response.text.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,'')
-         let scriptTexto, scriptEjecutable
+          try {
 
-         
-         /* Revisión textual */
-         try {
-            scriptTexto = [` arreglo = document.getElementsByClassName('profile-card-loc__txt')`,`elemento = arreglo[0]`, `elemento.textContent = 'Istanbul, Turkey'`]
-            for(let elemento of scriptTexto) {
-              chai.expect(responseClean).to.contain(elemento.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,''))
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+            
+            for(let elemento of script2) {
+              chai.expect(responseclean).to.contain(elemento.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,''))
             }
             
           } catch (error) {
-            chai.expect.fail('Revise la petición por clase y la modificación del contenido del elemento');
+            chai.expect.fail(`Código esperado: 
+              
+              ${script2.join('\n')}
+              
+              `);
             return;
           }
-
-            
+        
         })
-    }); 
+    });
 
 
-    it('getElementsByTagName', function() {
+    it(`En public/scripts/ejercicio.js con el código:
+
+      ${script3.join('\n')}
+      
+      `, function() {
       return request(app)
         .get('/scripts/ejercicio.js')
         .then((response) => {
 
-         let responseClean = response.text.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,'')
-         let scriptTexto, scriptEjecutable
+          try {
 
-         
-         /* Revisión textual */
-         try {
-            scriptTexto = [`arreglo = document.getElementsByTagName('img')`,`elemento = arreglo[0]`, `elemento.setAttribute('src','https://res.cloudinary.com/muhammederdem/image/upload/v1537638518/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.jpg')`]
-            for(let elemento of scriptTexto) {
-              chai.expect(responseClean).to.contain(elemento.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,''))
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+            
+            for(let elemento of script3) {
+              chai.expect(responseclean).to.contain(elemento.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,''))
             }
             
           } catch (error) {
-            chai.expect.fail('Revise la petición por clase y la modificación del contenido del elemento');
+            chai.expect.fail(`Código esperado: 
+              
+              ${script3.join('\n')}
+              
+              `);
             return;
           }
-
-            
+        
         })
-    }); 
+    });
 
 
-    it('querySelector', function() {
+    it(`En public/scripts/ejercicio.js con el código:
+
+      ${script4.join('\n')}
+      
+      `, function() {
       return request(app)
         .get('/scripts/ejercicio.js')
         .then((response) => {
 
-         let responseClean = response.text.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,'')
-         let scriptTexto, scriptEjecutable
+          try {
 
-         
-         /* Revisión textual */
-         try {
-            scriptTexto = [`elemento = document.querySelector('div.profile-card-inf')`,` elemento.innerHTML = textoHTML`]
-            for(let elemento of scriptTexto) {
-              chai.expect(responseClean).to.contain(elemento.replace(/(\r\n|\n|\r)/gm, '').replace(/ /gm,''))
+            let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,'')
+            
+            for(let elemento of script4) {
+              chai.expect(responseclean).to.contain(elemento.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '').replace(/<br\s*[\/]?>/gi,'').replace(/<\/br\s*[\/]?>/gi,''))
             }
             
           } catch (error) {
-            chai.expect.fail('Revise la petición por clase y la modificación del contenido del elemento');
+            chai.expect.fail(`Código esperado: 
+              
+              ${script4.join('\n')}
+              
+              `);
             return;
           }
-
-            
+        
         })
-    }); 
-
-
+    });
     
  
 
