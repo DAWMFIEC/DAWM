1311147b7aaab383edbbdedbbef4521fc235f2cb
Allan <aavendan@gmail.com>
2022-11-26 11:17:46 -0500

    C04/EJ05

--- Capítulo 04/ejercicio05/specs/redes.component.spec.ts
+++ Capítulo 04/ejercicio05/specs/redes.component.spec.ts
@@ -23,21 +23,30 @@ describe('RedesComponent', () => {
 
   it("should has attribute redes",()=>{
     let redesTmp: any[] = [
-      {
-        'href': 'https://twitter.com/home',
-        'alt': 'Sígueme en Twitter',
-        'src': 'https://cdn-icons-png.flaticon.com/512/733/733579.png'
-      },
-      {
-        'href': 'https://www.facebook.com/',
-        'alt': 'Likes en Facebook',
-        'src': 'https://cdn-icons-png.flaticon.com/512/174/174848.png'
-      },
-      {
-        'href': 'mailto:correo@gmail.com',
-        'alt': 'Envíame un correo',
-        'src': 'https://cdn-icons-png.flaticon.com/512/2111/2111463.png'
-      }
+       {
+         'href': 'https://twitter.com/home',
+         'alt': 'Sígueme en Twitter',
+         'src': 'https://cdn-icons-png.flaticon.com/512/733/733579.png',
+         'show': true
+       },
+       {
+         'href': 'https://www.facebook.com/',
+         'alt': 'Likes en Facebook',
+         'src': 'https://cdn-icons-png.flaticon.com/512/174/174848.png',
+         'show': true
+       },
+       {
+         'href': 'mailto:correo@gmail.com',
+         'alt': 'Envíame un correo',
+         'src': 'https://cdn-icons-png.flaticon.com/512/542/542638.png',
+         'show': false
+       },
+       {
+         'href': 'https://www.instagram.com/',
+         'alt': 'Likes en Instagram',
+         'src': 'https://cdn-icons-png.flaticon.com/512/2111/2111463.png',
+         'show': true
+       }
     ];
     let redes: any[] = component.redes;
     expect(redes).toEqual(redesTmp);
