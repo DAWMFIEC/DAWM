aa1f9935f8eb49cc6fb4ace2f8ce510220566b50
aavendan <aavenda@gmail.com>
2024-05-18 13:00:44 -0500

    C02E04

---
+++ Capítulo02/ejercicio04/README.md
@@ -0,0 +1,43 @@
+# Ejercicio 04
+
+#### Dispositivo Fijo
+
+![image info](images/ejercicio04-1.gif)
+
+#### Dispositivo Móvil (Ancho mínimo: 576px)
+
+![image info](images/ejercicio04-2.gif)
+
+
+## Instrucciones
+
+* Descargue y descomprima [C02E04.zip](../../zips/C02E04.zip)
+* Desde la línea de comandos
+	+ Ingrese a la carpeta con el ejercicio
+	+ Instale los paquetes necesarios, con: `npm install`
+	+ Levante el servidor con cualquiera de las siguientes opciones:
+		- En modo predeterminado, con: `npm start`
+		- En modo de depuración 
+			+ En Windows, con: `npm run debugw`
+			+ En MacOs, con: `npm run debugm`
+	+ Utilice su navegador preferido para abrir la vista inicial del ejercicio, con el URL: `http://localhost:3000/`
+
+* Desde el navegador, revise cada una de las instrucciones que aparecen en los recuadros de color verde.
+* Desde el editor de texto, modifica el archivo `views/index.ejs`
+	+ Agrega tu respuesta dentro de cada uno de los comentarios marcados, con:  
+	`<!-- Inicio -->` y `<!-- Fin -->`
+* Desde el navegador, actualice la página y verifique si los cambios cumplen con las instrucciones solicitadas.
+
+## Pruebas unitarias
+
+* Desde otra instancia de la línea de comandos, ejecute: `npm test`
+* Los resultados posibles a las pruebas unitarias pueden ser: 
+	+ Todos fueron exitosas, o
+	+ Existen pruebas unitarias fallidas.
+* En caso de ser necesario, modifique el/los archivo(s) y vuelva a ejecutar las pruebas unitarias. 
+
+## Referencias 
+
+* Mark Otto, J. T. (n.d.). Bootstrap. Retrieved from https://getbootstrap.com/
+* golapongolapon                      2122 bronze badges, Luka KraljLuka Kralj                      43633 silver badges1212 bronze badges, Matteo PaganiMatteo Pagani                      54555 silver badges1414 bronze badges, & ggorlenggorlen                      52.4k77 gold badges9595 silver badges133133 bronze badges. (1965). How to expect that a string contains exactly N of a particular character in Chai? Retrieved from https://stackoverflow.com/questions/55985716/how-to-expect-that-a-string-contains-exactly-n-of-a-particular-character-in-chai
+* somebodyelsesomebodyelse                      76111 gold badge66 silver badges88 bronze badges, user113716user113716                      321k6464 gold badges452452 silver badges441441 bronze badges, HoldOffHungerHoldOffHunger                      19.9k1111 gold badges111111 silver badges140140 bronze badges, & SnowcatSnowcat                      49999 silver badges1818 bronze badges. (1957). How to put variable in regular expression match? Retrieved from https://stackoverflow.com/questions/7053506/how-to-put-variable-in-regular-expression-match
---
+++ Capítulo02/ejercicio04/test/bootstrap.test.js
@@ -0,0 +1,190 @@
+const assert = require('assert');
+const chai = require('chai');
+const request = require('supertest');
+const app = require('../app');
+
+
+describe('Test unitarios para la ruta `/`', function () {
+
+    it('Respuesta del servidor', function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+                assert.equal(response.status, 200)
+            })
+    });
+
+    let meta = '<meta name="viewport" content="width=device-width, initial-scale=1">'
+
+    it('En views/index.ejs use las etiquetas: ' + meta, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let typetextclean = meta.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(typetextclean);
+
+            })
+    });
+
+    let styletag = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"`
+    let scripttag = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"`
+    let bootc1 = `<div id="div-1" class="container py-3">`
+    let bootc2 = `<div id="div-2" class="row row-cols-sm-1 row-cols-md-3 mb-3 text-center">`
+    let bootc31 = `<div id="div-3-1" class="col">`
+    let bootc32 = `<div id="div-3-2" class="col">`
+    let bootc33 = `<div id="div-3-3" class="col">`
+    let bootc41 = `<div id="div-4-1" class="card mb-4 rounded-3 shadow`
+    let bootc42 = `<div id="div-4-2" class="card mb-4 rounded-3 shadow`
+    let bootc43 = `<div id="div-4-3" class="card mb-4 rounded-3 shadow`
+    let bootbtn = `<button type="button" class="w-100 btn`
+
+    it('En views/index.ejs use la etiqueta: ' + styletag, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let styletagclean = styletag.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(styletagclean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + scripttag, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let scripttagclean = scripttag.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(scripttagclean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc1, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc1clean = bootc1.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc1clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc2, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc2clean = bootc2.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc2clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc31, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc31clean = bootc31.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc31clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc32, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc32clean = bootc32.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc32clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc33, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc33clean = bootc33.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc33clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc41, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc41clean = bootc41.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc41clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc42, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc42clean = bootc42.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc42clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootc43, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootc43clean = bootc43.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootc43clean);
+
+            })
+    });
+
+    it('En views/index.ejs use la etiqueta: ' + bootbtn, function () {
+        return request(app)
+            .get('/')
+            .then((response) => {
+
+                let responseclean = response.text.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+                let bootbtnclean = bootbtn.replace(/\s/g, '').replace(/(?:\r\n|\r|\n)/g, '')
+
+                chai.expect(responseclean).to.contain(bootbtnclean);
+
+                let re = new RegExp(bootbtnclean, 'g');
+                let countclean = (responseclean.match(re) || []).length
+
+                chai.expect(countclean).to.equal(3)
+            })
+    });
+});
