484b7a72d653cc4cbc1246f061ceb7fc53b5e7a4
aavendan <aavenda@gmail.com>
2024-05-18 13:14:19 -0500

    C02E04 tests

--- Capítulo02/ejercicio04/test/bootstrap.test.js
+++ Capítulo02/ejercicio04/test/bootstrap.test.js
@@ -29,8 +29,8 @@ describe('Test unitarios para la ruta `/`', function () {
             })
     });
 
-    let styletag = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"`
-    let scripttag = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"`
+    let styletag = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">`
+    let scripttag = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>`
     let bootc1 = `<div id="div-1" class="container py-3">`
     let bootc2 = `<div id="div-2" class="row row-cols-sm-1 row-cols-md-3 mb-3 text-center">`
     let bootc31 = `<div id="div-3-1" class="col">`
@@ -39,7 +39,7 @@ describe('Test unitarios para la ruta `/`', function () {
     let bootc41 = `<div id="div-4-1" class="card mb-4 rounded-3 shadow`
     let bootc42 = `<div id="div-4-2" class="card mb-4 rounded-3 shadow`
     let bootc43 = `<div id="div-4-3" class="card mb-4 rounded-3 shadow`
-    let bootbtn = `<button type="button" class="w-100 btn`
+    let bootbtn = `<button type="button" class="w-100 btn btn-lg`
 
     it('En views/index.ejs use la etiqueta: ' + styletag, function () {
         return request(app)
