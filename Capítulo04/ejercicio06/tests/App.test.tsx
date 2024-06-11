9b1e23ae18385c5709d1ed091cb370f2546184df
aavendan <aavenda@gmail.com>
2024-06-11 11:31:07 -0500

    C04E06

---
+++ Capítulo04/ejercicio06/tests/App.test.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom/vitest';
+import { afterEach, describe, it, expect } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react'
+import App from './App'
+
+
+describe('App', () => {
+
+    afterEach(cleanup);
+
+    it('render App', () => {
+        render(<App />)
+    })
+
+    it('render Calculator Component', () => {
+        render(<App />)
+        const calculatorComponent = screen.getByText("Calculator Component");
+        expect(calculatorComponent).toBeInTheDocument();
+    })
+
+    it('render Plan Component', () => {
+        render(<App />)
+        const planComponent = screen.getByText("Plan Component");
+        expect(planComponent).toBeInTheDocument();
+    })
+
+    it('render Result Component', () => {
+        render(<App />)
+        const resultComponent = screen.getByText("Result Component");
+        expect(resultComponent).toBeInTheDocument();
+    })
+})
